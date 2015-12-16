5b81cab3bfff46ed73b604244b5e12540893785f
Tyler Kovar <gkovar@gmail.com>
2015-12-16 16:23:35 -0500

    forgot to branch...

--- WP_Places.php
+++ WP_Places.php
@@ -25,7 +25,21 @@
  *	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+
+
+//http://www.yaconiello.com/blog/how-to-handle-wordpress-settings/
 require_once(dirname(__FILE__) . "/includes/googlePlaces.php");
+
+
+function WP_Places_menu() {
+	add_options_page( 'WP_Places Page', 'WP_Places', 'manage_options', 'wp-places-plugin', 'WP_Places_add_settings_field' );
+}
+add_action('admin_menu','WP_Places_menu');
+
+
+
+
+
 
 
 //Get the users google places key
