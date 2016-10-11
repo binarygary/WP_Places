58847c4bce459b597ccd0b81efc5dad5d1f00ec1
Gary Kovar <gkovar@gmail.com>
2016-10-10 22:19:29 -0400

    fixed some output issues; created get_standard_address method to display a nice version of place and address

--- includes/class-place-data.php
+++ includes/class-place-data.php
@@ -45,29 +45,31 @@ class WPP_Place_Data {
 	}
 
 	public function load_place( $place_id ) {
-		if ( false === ( $this->raw_results = get_transient( "_WP_Places_$place_id" ) ) ) {
+//		if ( false === ( $this->raw_results = get_transient( "_WP_Places_$place_id" ) ) ) {
 			$this->raw_results = $this->plugin->google_places_api->placeDetails( $place_id );
-			set_transient( "_WP_Places_$place_id", $this->raw_results, MINUTE_IN_SECONDS );
-		}
-		$this->hours              = isset( $this->raw_results[ 'result' ][ 'opening_hours' ][ 'weekday_text' ] ) ? $this->raw_results[ 'result' ][ 'opening_hours' ][ 'weekday_text' ] : '';//
-		$this->open_now           = isset( $this->raw_results[ 'result' ][ 'opening_hours' ][ 'open_now' ] ) ? $this->raw_results[ 'result' ][ 'opening_hours' ][ 'open_now' ] : '';
-		$this->priceLevel         = isset( $this->raw_results[ 'result' ][ 'price_level' ] ) ? $this->raw_results[ 'result' ][ 'price_level' ] : '';
-		$this->name               = isset( $this->raw_results[ 'result' ][ 'name' ] ) ? $this->raw_results[ 'result' ][ 'name' ] : '';//
-		$this->rating             = isset( $this->raw_results[ 'result' ][ 'rating' ] ) ? $this->raw_results[ 'result' ][ 'rating' ] : '';
-		$this->phone_number       = isset( $this->raw_results[ 'result' ][ 'formatted_phone_number' ] ) ? $this->raw_results[ 'result' ][ 'formatted_phone_number' ] : '';//
-		$this->website            = isset( $this->raw_results[ 'result' ][ 'website' ] ) ? $this->raw_results[ 'result' ][ 'website' ] : '';//
-		$this->lat                = isset( $this->raw_results[ 'result' ][ 'geometry' ][ 'location' ][ 'lat' ] ) ? $this->raw_results[ 'result' ][ 'geometry' ][ 'location' ][ 'lat' ] : '';
-		$this->lng                = isset( $this->raw_results[ 'result' ][ 'geometry' ][ 'location' ][ 'lng' ] ) ? $this->raw_results[ 'result' ][ 'geometry' ][ 'location' ][ 'lng' ] : '';
-		$this->formatted_address  = isset( $this->raw_results[ 'result' ][ 'formatted_address' ] ) ? $this->raw_results[ 'result' ][ 'formatted_address' ] : '';//
-		$this->permanently_closed = isset( $this->raw_results[ 'result' ][ 'permanently_closed' ] ) ? $this->raw_results[ 'result' ][ 'permanently_closed' ] : '';
-		$this->reviews            = isset( $this->raw_results[ 'result' ][ 'reviews' ] ) ? $this->raw_results[ 'result' ][ 'reviews' ] : '';
-		$this->photos             = isset( $this->raw_results[ 'result' ][ 'photos' ] ) ? $this->raw_results[ 'result' ][ 'photos' ] : '';
+//			set_transient( "_WP_Places_$place_id", $this->raw_results, MINUTE_IN_SECONDS );
+//		}
+
+		$this->hours              = isset( $this->raw_results[ 'opening_hours' ][ 'weekday_text' ] ) ? $this->raw_results[ 'opening_hours' ][ 'weekday_text' ] : '';//
+		$this->open_now           = isset( $this->raw_results[ 'opening_hours' ][ 'open_now' ] ) ? $this->raw_results[ 'opening_hours' ][ 'open_now' ] : '';
+		$this->priceLevel         = isset( $this->raw_results[ 'price_level' ] ) ? $this->raw_results[ 'price_level' ] : '';
+		$this->name               = isset( $this->raw_results[ 'name' ] ) ? $this->raw_results[ 'name' ] : '';//
+		$this->rating             = isset( $this->raw_results[ 'rating' ] ) ? $this->raw_results[ 'rating' ] : '';
+		$this->phone_number       = isset( $this->raw_results[ 'formatted_phone_number' ] ) ? $this->raw_results[ 'formatted_phone_number' ] : '';//
+		$this->website            = isset( $this->raw_results[ 'website' ] ) ? $this->raw_results[ 'website' ] : '';//
+		$this->lat                = isset( $this->raw_results[ 'geometry' ][ 'location' ][ 'lat' ] ) ? $this->raw_results[ 'geometry' ][ 'location' ][ 'lat' ] : '';
+		$this->lng                = isset( $this->raw_results[ 'geometry' ][ 'location' ][ 'lng' ] ) ? $this->raw_results[ 'geometry' ][ 'location' ][ 'lng' ] : '';
+		$this->formatted_address  = isset( $this->raw_results[ 'formattedAddress' ] ) ? $this->raw_results[ 'formattedAddress' ] : '';//
+		$this->permanently_closed = isset( $this->raw_results[ 'permanently_closed' ] ) ? $this->raw_results[ 'permanently_closed' ] : '';
+		$this->reviews            = isset( $this->raw_results[ 'reviews' ] ) ? $this->raw_results[ 'reviews' ] : '';
+		$this->photos             = isset( $this->raw_results[ 'photos' ] ) ? $this->raw_results[ 'photos' ] : '';
 	}
 
 	public function get_standard_address( $place_id ) {
 		if (!isset($this->raw_results)){
 			$this->load_place($place_id);
 		}
-		return $this->name . ' ' . $this->formatted_address;
+		$display_name = $this->name . ', ' . $this->formatted_address;
+		return $display_name;
 	}
 }
