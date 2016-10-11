1317cfdcd6e98455da955041ceab4c42862eda7e
Gary Kovar <gkovar@gmail.com>
2016-10-10 23:11:41 -0400

    finished porting over all legacy code

--- includes/class-content.php
+++ includes/class-content.php
@@ -2,7 +2,7 @@
 /**
  * WP_Places Content
  *
- * @since NEXT
+ * @since   NEXT
  * @package WP_Places
  */
 
@@ -12,6 +12,7 @@
  * @since NEXT
  */
 class WPP_Content {
+
 	/**
 	 * Parent plugin class
 	 *
@@ -24,7 +25,9 @@ class WPP_Content {
 	 * Constructor
 	 *
 	 * @since  NEXT
+	 *
 	 * @param  object $plugin Main plugin object.
+	 *
 	 * @return void
 	 */
 	public function __construct( $plugin ) {
@@ -39,5 +42,86 @@ class WPP_Content {
 	 * @return void
 	 */
 	public function hooks() {
+		if ( $this->plugin->settings->embed_div() ) {
+			add_filter( 'the_content', array( $this, 'WP_Places_add_before_content' ) );
+		}
+	}
+
+	function WP_Places_add_before_content($content) {
+		$locationPlace=get_post_meta(get_the_ID(),'_wp_places', true);
+		//let's go ahead and cache this
+
+		if ( false === ( $placeArray = get_transient( "_Wp_Places_$locationPlace" ) ) )  {
+			$placeArray = $this->plugin->google_places_api->placeDetails($locationPlace);
+			set_transient( "_Wp_Places_$locationPlace", $placeArray, MINUTE_IN_SECONDS );
+		}
+
+
+		if (!is_array($placeArray)) {
+			return $content;
+		}
+
+		if(array_key_exists('name',$placeArray)) {
+			$style=get_option( 'WP_Places_CSS', '' );
+			$WpPlaces="<DIV style=\"$style\">";
+
+			if (array_key_exists('openNow',$placeArray)) {
+				if ($placeArray['openNow']==1) {
+					$WpPlaces.="<span style=\"color: red;\">Open Now</SPAN><BR>";
+				}
+			}
+
+
+
+			if (array_key_exists('permanentlyClosed',$placeArray)) {
+				if ($placeArray['permanentlyClosed']==1) {
+					$WpPlaces.="<span style=\"color: red;\">PERMANENTLY CLOSED</SPAN><BR>";
+				}
+			}
+
+			$WpPlaces.="<B>$placeArray[name]</B><BR>";
+
+			if (array_key_exists('formattedAddress',$placeArray)) {
+				$WpPlaces.="<div itemprop=address itemscope itemtype=http://schema.org/PostalAddress>$placeArray[formattedAddress]<BR></div>";
+			}
+			if (array_key_exists('phoneNumber',$placeArray)) {
+				$WpPlaces.="<span itemprop=telephone>$placeArray[phoneNumber]</span><BR>";
+			}
+			if (array_key_exists('hours',$placeArray)) {
+				if (is_array($placeArray['hours'])) {
+					foreach ($placeArray['hours'] as $day) {
+						$WpPlaces.="$day<BR>";
+					}
+				}
+			}
+			if (array_key_exists('website',$placeArray)) {
+				$WpPlaces.="<a href=$placeArray[website] itemprop=url>website</a><BR>";
+			}
+			if(get_option('WP_Places_Google_Attr_Setting_check')=='googlecheck') {
+				$WpPlaces.="<img src=".plugins_url('img/powered_by_google_on_white.png', __FILE__) . ">";
+			}
+			$WpPlaces.="</DIV>";
+		}
+		if (is_single()) {
+			$contents = explode("</p>", $content);
+			if (is_array($contents)) {
+				$added=0;
+				foreach ($contents as $paragraph) {
+					if ($added!=1) {
+						$paragraph=$paragraph.$WpPlaces;
+						$added=1;
+						$content=null;
+					}
+					$content.=$paragraph."</p>";
+				}
+			} else {
+				$content=$WpPlaces.$content;
+			}
+
+
+			return $content;
+		} else {
+			return $content;
+		}
 	}
 }
--- includes/class-shortcodes.php
+++ includes/class-shortcodes.php
@@ -2,7 +2,7 @@
 /**
  * WP_Places Shortcodes
  *
- * @since NEXT
+ * @since   NEXT
  * @package WP_Places
  */
 
@@ -12,6 +12,7 @@
  * @since NEXT
  */
 class WPP_Shortcodes {
+
 	/**
 	 * Parent plugin class
 	 *
@@ -24,7 +25,9 @@ class WPP_Shortcodes {
 	 * Constructor
 	 *
 	 * @since  NEXT
+	 *
 	 * @param  object $plugin Main plugin object.
+	 *
 	 * @return void
 	 */
 	public function __construct( $plugin ) {
@@ -39,5 +42,97 @@ class WPP_Shortcodes {
 	 * @return void
 	 */
 	public function hooks() {
+		add_shortcode( 'wp_places_search', array( $this, 'WP_Places_search_shortcode' ) );
+		add_shortcode( 'wp_places', array( $this, 'WP_Places_shortcode' ) );
 	}
+
+
+	function WP_Places_search_shortcode( $attr ) {
+		$post_id = get_the_ID();
+		foreach ( $attr as $word => $value ) {
+			if ( $word == 'lat' ) {
+				update_post_meta( $post_id, '_WP_Places_meta_value_lat', $value );
+			} elseif ( $word == 'lon' ) {
+				update_post_meta( $post_id, '_WP_Places_meta_value_lon', $value );
+			} else {
+				$phrase = $phrase . " " . $value;
+			}
+		}
+		update_post_meta( $post_id, '_WP_Places_meta_value_key', $phrase );
+		if ( get_post_meta( get_the_ID(), '_WP_Places_meta_value_lat', true ) ) {
+			$result = $this->google_places_api->searchGPS( $phrase, get_post_meta( get_the_ID(), '_WP_Places_meta_value_lat', true ), get_post_meta( get_the_ID(), '_WP_Places_meta_value_lon', true ) );
+		} else {
+			$result = $this->google_places_api->search( $phrase );
+		}
+		update_post_meta( $post_id, '_WP_Places_meta_Google_response', $result );
+	}
+
+
+	function WP_Places_shortcode( $attr ) {
+		$apikey          = get_option( 'WP_Places_Google_Id_Setting' );
+		$locationPlace   = get_post_meta( get_the_ID(), '_WP_Places_meta_Google_response', true );
+		$placeArray      = $this->google_places_api->placeDetails( $locationPlace );
+		$attributesArray = array(
+			"openNow",
+			"openNowText",
+			"permanentlyClosed",
+			"name",
+			"formattedAddress",
+			"phoneNumber",
+			"hours",
+			"website",
+			"priceLevel",
+			"rating",
+			"lat",
+			"lng",
+			"reviews",
+			"photos",
+		);
+		foreach ( $attr as $index => $key ) {
+			if ( in_array( $key, $attributesArray ) ) {
+				if ( "hours" == $key ) {
+					$hoursList = "<UL>";
+					if ( count( $placeArray[ $key ] ) > 1 ) {
+						foreach ( $placeArray[ $key ] as $hoursOfOperation ) {
+							$hoursList .= "<LI>$hoursOfOperation";
+						}
+					}
+					$hoursList .= "</UL>";
+
+					return $hoursList;
+				} elseif ( "reviews" == $key ) {
+					if ( ! is_array( $placeArray[ 'reviews' ] ) ) {
+						return null;
+					}
+					//print_r($placeArray[reviews]);
+					$reviewData = "<UL>";
+					foreach ( $placeArray[ 'reviews' ] as $review ) {
+						$reviewData .= "<LI><B>" . $review[ 'rating' ] . " out of 5</B> " . $review[ 'text' ] . " by <i><a href=$review[author_url]>$review[author_name]</i></a></LI>";
+					}
+					$reviewData .= "<UL>";
+
+					return $reviewData;
+				} elseif ( "photos" == $key ) {
+					if ( ! is_array( $placeArray[ 'photos' ] ) ) {
+						return null;
+					}
+					foreach ( $placeArray[ 'photos' ] as $photo ) {
+						$photoList .= "<img src=https://maps.googleapis.com/maps/api/place/photo?photoreference=$photo[photo_reference]&maxwidth=1024&key=$apikey>";
+					}
+
+					return $photoList;
+				} elseif ( "openNowText" == $key ) {
+
+					if ( $placeArray[ 'openNow' ] == 1 ) {
+						return "Open Now!";
+					}
+
+				} else {
+					return $placeArray[ $key ];
+				}
+			}
+		}
+	}
+
+
 }
